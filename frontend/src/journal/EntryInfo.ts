d9e3e881035a19abbf82046420ecd21470f95a40
Alex <alexandra_hogue@brown.edu>
2023-12-02 14:31:58 -0500

    feat: small edits to EntryInfo

--- frontend/src/journal/EntryInfo.ts
+++ frontend/src/journal/EntryInfo.ts
@@ -1,11 +1,14 @@
 export class EntryInfo {
+  prompt: string;
   date: string;
   entry: string | undefined;
 
   constructor(
+    prompt: string,
     date: string,
     entry: string | undefined
   ) {
+    this.prompt = prompt;
     this.date = date;
     this.entry = entry;
   }
