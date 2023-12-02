43b43c46d025e8ae663e0d906fb14f77d2d69cde
selenajoetwilliams <selenajoetwilliams@gmail.com>
2023-12-02 16:05:32 -0500

    [feat/fix] prmopt fetching is integrated through front and back ends

--- frontend/src/journal/JournalPrompt.tsx
+++ frontend/src/journal/JournalPrompt.tsx
@@ -3,28 +3,30 @@ import { Dispatch, SetStateAction, useState, useEffect } from "react";
 
 export default function JournalPrompt() {
 
-  const [prompt, setPrompt] = useState<string>("");
+  const [prompt, setPrompt] = useState<String | undefined>("");
 
-  async function fetchPrompt(): Promise<String | void> {
+  async function fetchPrompt(): Promise<String | undefined> {
     const link =  "http://localhost:3232/getprompt";
 
   return fetch(link)
     .then((response) => response.json())
     .then((responseObject) => {
       console.log("response object " + responseObject);
+      console.log("PROMPT: " + responseObject.prompt)
       return responseObject.prompt;
     });
 
 
   }
 
-  const fetchData = async () => {
-    const prompt = await fetchPrompt();
-
-  if (typeof prompt === "string") {
+  /* gets the prompt when the web page initializes */
+  useEffect(() => {
+    const fetchData = async () => {
+      const prompt = await fetchPrompt();
       setPrompt(prompt);
-    }
-  }
+    } 
+    fetchData();
+  });
 
   console.log("prompt is " + prompt)
   return (
