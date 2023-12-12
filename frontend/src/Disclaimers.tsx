3a5d701cc38972863b8ae5ba6d7b2ceaba7849f2
Alex <alexandra_hogue@brown.edu>
2023-12-12 15:08:07 -0500

    feat: styling change for disclaimer message

--- frontend/src/Disclaimers.tsx
+++ frontend/src/Disclaimers.tsx
@@ -1,21 +1,22 @@
-// a wrapper component to list disclaimers 
+import "./styles/suggestions.css";
 
+// a wrapper component to list disclaimers
 
 // e.g.: data privacy discalimer
 // medical disclaimer & hotline phone #
 export default function disclaimers() {
-    return (
-      <div className="disclaimers">
-        <p>
-          <b>Note on the suggestions:</b> We use your journal response to
-          generate personalized suggestions using an open-source large language
-          model and a recommendation algorithm
-        </p>
-
-        <p>
-          This is not a substitute for medical support. If you are in crisis,
-          please call the national mental health hotline at (806) 903-3787
-        </p>
-      </div>
-    );
+  return (
+    <div className="disclaimers">
+      <p className="initial-message">Hover to view disclaimer!</p>
+      <p className="hover-text">
+        <b>Note on the suggestions:</b> We use your journal response to
+        generate personalized suggestions using an open-source large language
+        model and a recommendation algorithm
+      </p>
+      <p className="hover-text">
+        This is not a substitute for medical support. If you are in crisis,
+        please call the national mental health hotline at (806) 903-3787
+      </p>
+    </div>
+  );
 }
